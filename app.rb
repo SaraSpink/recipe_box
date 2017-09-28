eeaf0ae4a4f57ddb603bac61a2b82d29485c109a
Qudsia Ahmad and Sara Spink <qudsia@>
2017-09-28 16:11:58 -0700

    be able to add ingredients to a recipe

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+# get ("/") do
+#   @recipes = Recipe.all()
+#   erb(:index)
+# end
+#
+# post ("/recipe") do
+#   name = params["name"]
+#   @recipes = Recipe.create({:name => name, :instructions => nil, :rating => nil})
+#   erb(:index)
+# end
+
+
+# get ("/recipe/:id")
+#   @name = Recipe.all()
+#   @recipe_by_id = Recipe.create({})
+#   erb(:recipe_detail)
+# end
+
 get ("/") do
   @recipes = Recipe.all()
   erb(:index)
@@ -17,9 +35,14 @@ end
 
 get ("/recipe/:id") do
   @recipe_by_id = Recipe.find(params.fetch(:id).to_i())
+
   erb(:recipe_detail)
 end
 
 post ("/recipe/:id") do
-  
+  @recipe_by_id = Recipe.find(params.fetch(:id).to_i())
+  ingredient = Ingredient.create({:name => params["ingredient"]})
+  @recipe_by_id.ingredients.push(ingredient)
+
+  erb(:recipe_detail)
 end
